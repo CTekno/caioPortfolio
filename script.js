a9045a1d5af0b663a89d6f7335efa82a3f05a128
CaioT <caio.m.taparo@gmail.com>
2024-10-02 15:06:41 -0300

    portfolio

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const modal = document.getElementById(modalId)
+const botao1 = document.querySelector("modal1")
+const botao2 = document.querySelector("modal2")
+const botao3 = document.querySelector("modal3")
+const botao4 = document.querySelector("modal4")
+
+function abc(modalId) {
+  document.getElementById(modalId).showModal()
+}
+
+function closemodal(modalId) {
+  document.getElementById(modalId).close();
+}
+
+function jdv() {
+  location.href = 'https://jogodavelha2.vercel.app'
+}
+
+function estrpd() {
+  location.href = 'https://estoquerapido.vercel.app'
+}
+
+function port() {
+  location.href = 'https://oscartaparojunior.vercel.app'
+}
